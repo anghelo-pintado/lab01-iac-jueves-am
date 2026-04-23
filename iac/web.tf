8b903c46679fcbb4ab206ede42a5d86ae5b5d200
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-04-23 18:19:10 -0500

    feat(fuera de tiempo, no considerar): Se modifico el docker compose para crear las imagenes de manera automatica con un nombre predeterminado

--- iac/web.tf
+++ iac/web.tf
@@ -1,7 +1,7 @@
 # Start a container
 resource "docker_container" "web" {
   name  = "web-${terraform.workspace}"
-  image = "lab01-iac-jueves-am-web01"
+  image = "lab/web01"
 
   ports {
     internal = 80
